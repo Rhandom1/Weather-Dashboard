157ba1db34310999e08a1d620200b852bb23d327
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-07-22 13:25:47 -0500

    Restructured the layout, added some styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,9 @@
-// store the search value
-let city = $("#search").val();
+var searchButton = $('#searchBtn');
+var searchEl = $('.list-group');
+
 // add api?
 const apiKey = "46b821e4cbd17e00a860614cfb6899bc";
+
+$("#searchBtn").click(function(){
+    
+})
